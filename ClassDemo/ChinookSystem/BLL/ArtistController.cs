8cbf60dbdb500be478f583913303af0aab513749
Dane Christenson <danechristenson@gmail.com>
2017-09-22 20:21:42 -0600

    Added components for ODS wizard

--- ClassDemo/ChinookSystem/BLL/ArtistController.cs
+++ ClassDemo/ChinookSystem/BLL/ArtistController.cs
@@ -1,15 +1,19 @@
 ﻿using Chinook.Data.Entities;
 using ChinookSystem.DAL;
+using System.ComponentModel; // expose methods for ODS wizard
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ChinookSystem.BLL
 {
+    [DataObject]
     public class ArtistController
     {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
         public List<Artist> Artists_List()
         {
             using (var context = new ChinookContext())
