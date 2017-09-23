bc26eca37af9c482de65bb85872a89031684058a
Dane Christenson <danechristenson@gmail.com>
2017-09-22 20:43:29 -0600

    Added FirstLinqSample site

--- ClassDemo/ChinookSystem/BLL/AlbumController.cs
+++ ClassDemo/ChinookSystem/BLL/AlbumController.cs
@@ -1,7 +1,29 @@
-﻿namespace ChinookSystem.BLL
+﻿using Chinook.Data.POCOs;
+using ChinookSystem.DAL;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace ChinookSystem.BLL
 {
-    class AlbumController
+    [DataObject]
+    public class AlbumController
     {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<ArtistAlbumByReleaseYear> Albums_ListForArtist(int artistId)
+        {
+            using (var context = new ChinookContext())
+            {
+                var results = from x in context.Albums
+                              where x.ArtistId.Equals(artistId)
+                              select new ArtistAlbumByReleaseYear
+                              {
+                                  Title = x.Title,
+                                  Released = x.ReleaseYear
+                              };
+                return results.ToList();
+            }
+        }
 
     }
 }
---
+++ ClassDemo/Website/SamplePages/FirstLinqSample.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="FirstLinqSample.aspx.cs" Inherits="SamplePages_FirstLinqSample" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="Server">
+
+    <h1>Albums for Artist</h1>
+    <asp:Label ID="Label1" runat="server" Text="Select an Artist"></asp:Label>
+    <asp:DropDownList ID="ArtistList" runat="server"></asp:DropDownList>
+    <asp:Button ID="Button1" runat="server" Text="Submit" />
+    <br />
+    <asp:GridView ID="ArtistAlbumList" runat="server"></asp:GridView>
+    <asp:ObjectDataSource ID="ArtistListODS" runat="server"></asp:ObjectDataSource>
+    <asp:ObjectDataSource ID="ArtistAlbumListODS" runat="server"></asp:ObjectDataSource>
+</asp:Content>
