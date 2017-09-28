c5677687960847e851f5bd8bb38e814fd53a3574
Dane Christenson <danechristenson@gmail.com>
2017-09-27 20:29:55 -0600

    Added album and genre DTO classes

---
+++ ClassDemo/Chinook.Data/DTOs/AlbumDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Data.DTOs
+{
+    public class AlbumDTO
+    {
+        public string title { get; set; }
+        public int releaseYear { get; set; }
+        public int numberOfTracks { get; set; }
+        public IEnumerable<TrackPOCO> tracks { get; set; }
+    }
+}
