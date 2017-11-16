3d0918feca4d918b158dd63e4f78e330d7825467
Dane Christenson <danechristenson@gmail.com>
2017-11-15 20:42:32 -0700

    Added up and down logic and BLL

--- ClassDemo/ChinookSystem/BLL/PlaylistTracksController.cs
+++ ClassDemo/ChinookSystem/BLL/PlaylistTracksController.cs
@@ -125,7 +125,78 @@ namespace ChinookSystem.BLL
             using (var context = new ChinookContext())
             {
                 //code to go here 
+                var exists = (from x in context.Playlists
+                              where x.UserName.Equals(username)
+                              && x.Name.Equals(playlistname)
+                              select x).FirstOrDefault();
+                if (exists == null)
+                {
+                    throw new Exception("Playlist has been removed from the file.");
+                }
+                else
+                {
+                    PlaylistTrack moveTrack = (from x in exists.PlaylistTracks
+                                               where x.TrackId == trackid
+                                               select x).FirstOrDefault();
 
+                    if(moveTrack == null)
+                    {
+                        throw new Exception("Playlist track has been removed from file.");
+                    }
+                    else
+                    {
+                        PlaylistTrack otherTrack = null;
+                        if (direction.Equals("up"))
+                        {
+                            if(moveTrack.TrackNumber == 1)
+                            {
+                                throw new Exception("Playlist track is already at the top of the playlist.");
+                            }
+                            else
+                            {
+                                otherTrack = (from x in exists.PlaylistTracks
+                                                           where x.TrackNumber == moveTrack.TrackNumber - 1
+                                                           select x).FirstOrDefault();
+                                if(otherTrack == null)
+                                {
+                                    throw new Exception("Playlist track has been removed from file.");
+                                }
+                                else
+                                {
+                                    moveTrack.TrackNumber--;
+                                    otherTrack.TrackNumber++;
+                                }
+                            }
+                        } 
+                        else
+                        {
+                            if (moveTrack.TrackNumber == exists.PlaylistTracks.Count())
+                            {
+                                throw new Exception("Playlist track is already at the bottom of the playlist.");
+                            }
+                            else
+                            {
+                                otherTrack = (from x in exists.PlaylistTracks
+                                              where x.TrackNumber == moveTrack.TrackNumber + 1
+                                              select x).FirstOrDefault();
+                                if (otherTrack == null)
+                                {
+                                    throw new Exception("Playlist track has been removed from file.");
+                                }
+                                else
+                                {
+                                    moveTrack.TrackNumber++;
+                                    otherTrack.TrackNumber--;
+                                }
+                            }
+                        }
+                        //staging
+                        context.Entry(moveTrack).Property(y => y.TrackNumber).IsModified = true; // changes only one thing
+                        context.Entry(otherTrack).Property(y => y.TrackNumber).IsModified = true;
+                        // saving
+                        context.SaveChanges();
+                    }
+                }
             }
         }//eom
 
