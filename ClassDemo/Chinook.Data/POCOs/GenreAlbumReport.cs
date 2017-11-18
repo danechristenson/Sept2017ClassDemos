2d42810dda7ef511263b23089c5fb250a3269b89
Dane Christenson <danechristenson@gmail.com>
2017-11-17 20:31:22 -0700

    Added Start of reporting

---
+++ ClassDemo/Chinook.Data/POCOs/GenreAlbumReport.cs
@@ -0,0 +1,12 @@
+﻿namespace Chinook.Data.POCOs
+{
+    public class GenreAlbumReport
+    {
+        public string GenreName { get; set; }
+        public string AlbumTitle { get; set; }
+        public string TrackName { get; set; }
+        public int Milliseconds { get; set; }
+        public int? Bytes { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
